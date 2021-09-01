db64835a18d3c043df108d81aded7ffcf6138959
PranaliPrakash <89651995+PranaliPrakash@users.noreply.github.com>
2021-09-01 13:10:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+#This is to clear the terminal and to display just the current state
 from replit import clear
 
 import random
 
-#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
+# Update the word list to use the 'word_list' from hangman_words.py
 #Delete this line: word_list = ["ardvark", "baboon", "camel"]
 from hangman_words import word_list
 
@@ -12,7 +13,7 @@ word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
-#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+# Import the logo from hangman_art.py and print it at the start of the game.
 from hangman_art import logo
 print(logo)
 
@@ -29,7 +30,7 @@ while not end_of_game:
     guess = input("Guess a letter: ").lower()
     clear()
 
-    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    # If the user has entered a letter they've already guessed, print the letter and let them know.
     if guess in display:
       print(f"The letter you guessed  {guess} is already guessed before")
 
@@ -42,7 +43,7 @@ while not end_of_game:
 
     #Check if user is wrong.
     if guess not in chosen_word:
-        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        # If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
         print(f"The letter you guessed {guess} , is not in the chosen_word  ")
         lives -= 1
         if lives == 0:
@@ -57,7 +58,7 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
-    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
+    # Import the stages from hangman_art.py and make this error go away.
     from hangman_art import stages
 
     print(stages[lives])
